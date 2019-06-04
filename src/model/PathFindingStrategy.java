1d1684da8e55544949335a748492a0c4f5a09e6c
Bamford <jordan.bamford@capgemini.com>
2019-06-04 23:12:58 +0100

    Adding in parameter ideas for calculatePath

--- src/model/PathFindingStrategy.java
+++ src/model/PathFindingStrategy.java
@@ -4,6 +4,6 @@ import java.util.ArrayList;
 
 public interface PathFindingStrategy {
 
-	public ArrayList<Location> calculatePath();
+	public ArrayList<Location> calculatePath(Location current, Location storageShelf, Location packingStation);
 	
 }
