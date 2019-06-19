797950f9dd9237ddc713ff6fcc6254486a0eb0a6
Bamford <jordan.bamford@capgemini.com>
2019-06-19 08:10:12 +0100

    Making initial PathFindingStategy changes and setup

--- src/model/PathFindingStrategy.java
+++ src/model/PathFindingStrategy.java
@@ -2,8 +2,18 @@ package model;
 
 import java.util.ArrayList;
 
-public interface PathFindingStrategy {
-
-	public Location getNextMove(Location target);
+public class PathFindingStrategy {
+	
+	//Needs access to the following data to calculate next best move:
+	//   -The location the robot is currently in
+	//   -The location that the robot is going to
+	//   -The location that other robots are in  (i.e. the locations we CAN move to)
+	
+	public Location getNextMove(Location currentPos, Location targetPos) {
+		
+		
+		
+		return null;
+	}
 	
 }
