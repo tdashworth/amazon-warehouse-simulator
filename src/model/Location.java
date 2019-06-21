8c0744cbabd265779d2d2ff18de043b8fbb3a4b9
Bamford <jordan.bamford@capgemini.com>
2019-06-21 13:09:30 +0100

    Swapping the constructors input variables as row and column were passed in the other way

--- src/model/Location.java
+++ src/model/Location.java
@@ -19,7 +19,7 @@ public class Location {
 	 * @param column
 	 * @throws Exception 
 	 */
-	public Location(int row, int column) throws LocationNotValidException {
+	public Location(int column, int row) throws LocationNotValidException {
 		if (row < 0 || column < 0)
 			throw new LocationNotValidException(row, column, "Row or column must not be less than 0.");
 		
